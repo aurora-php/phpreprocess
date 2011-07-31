5e804852a2ba4011a7e03e8a4e80cf1008151378
Harald Lapp <harald.lapp@gmail.com>
2011-08-01 01:33:20 +0200

    - initial commit plugin core class

---
+++ libs/plugin.class.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Plugin core class. Provides functionality useful for plugins.
+ *
+ * @octdoc      c:libs/plugin
+ * @copyright   copyright (c) 2011 by Harald Lapp
+ * @author      Harald Lapp <harald@octris.org>
+ */
+abstract class plugin
+/**/
+{
+    /**
+     * Plugin types.
+     * 
+     * @octdoc  d:plugin/T_UNDEFINED, T_FUNCTION, T_FILTER, T_CONDITION
+     */
+    const T_UNDEFINED = 1;
+    const T_FUNCTION  = 2;
+    const T_FILTER    = 3;
+    const T_CONDITION = 4;
+    /**/
+    
+    /**
+     * Plugin type.
+     *
+     * @octdoc  v:plugin/$type
+     * @var     int
+     */
+    protected static $type = self::T_UNDEFINED;
+    /**/
+    
+    /**
+     * Preprocessor instance.
+     *
+     * @octdoc  v:plugin/$preprocessor
+     * @var     preprocessor
+     */
+    private $preprocessor = null;
+    /**/
+    
+    /**
+     * Constructor.
+     *
+     * @octdoc  m:command/__construct
+     * @param   preprocessor    $prepro         Preprocessor instance
+     */
+    public function __construct(preprocessor $prepro)
+    /**/
+    {
+        $this->preprocessor = $prepro;
+    }
+    
+    /**
+     * Invoke must be implemented by sub-class.
+     *
+     * @octdoc  m:plugin/__invoke
+     * @param   array           $args           Arguments for plugin.
+     * @return  mixed                           Return value.
+     */
+    abstract public function __invoke(array $args);
+    /**/
+    
+    /**
+     * Return type of plugin.
+     *
+     * @octdoc  m:plugin/getType
+     * @return  int                             Type of plugin.
+     */
+    public static function getType()
+    /**/
+    {
+        return static::$type;
+    }
+}
